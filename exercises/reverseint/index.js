c2c8d532c2a3dba537ef7f6a7b8a5855bd403fb3
yukiyao119 <ukiyao@hotmail.com>
2020-04-03 02:12:57 -0400

    edited reverseint based on leetcode question

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -14,3 +14,26 @@ function reverseInt(n) {
 }
 
 module.exports = reverseInt;
+
+
+/* SOLUTION FOR LEETCODE REVERSE INTEGER, OVERFLOW */
+// var reverse = function(x) {
+//   let negative = false;
+
+//   if (x<0){
+//     negative = true
+//     x *= -1
+//   }
+
+//   let reversed = 0
+//   while(x>0){
+//     reversed = (reversed * 10) + (x % 10)
+//     x = Math.floor(x/10)
+//   }
+
+//   if (reversed > (2 ** 31 - 1)){
+//     return 0
+//   }
+
+//   return negative ? reversed * -1 : reversed
+// };
