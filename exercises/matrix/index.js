bcd4495431f63a70ad3f1d181462cb6afef04d65
yukiyao119 <ukiyao@hotmail.com>
2020-05-04 23:51:45 -0400

    matrix, spiral

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,54 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let results = [] // new Array(3).fill(0)
+  let counter = 1 // 1...n
+  
+  for (let i = 0; i < n; i++) {
+    results[i] = []
+  }
+  
+  let start_col = 0
+  let end_col = n-1
+  let start_row = 0
+  let end_row = n-1
+  
+  while( start_col <= end_col && start_row <= end_row) {
+    
+    //loop from start col to end col
+    for (let i = start_col; i <= end_col; i++ ) {
+      results[start_row][i] = counter //start_row is 0. this assigns the value for the first row of the matrix
+      counter++
+    }
+    
+    start_row++ //now go to the second row
+    
+    for( let i = start_row; i <= end_row; i++) { // loop from new start_row to end_row, start_col is still 0 to n-1
+      results[i][end_col] = counter
+      counter++
+    }
+    
+    end_col--
+    
+    for ( let i = end_col; i >= start_col; i--) {
+      results[end_row][i] = counter
+      counter++
+    }
+    
+    end_row--
+    
+    for (let i = end_row; i >= start_row; i--) {
+      results[i][start_col] = counter
+      counter++
+    }
+    
+    start_col++
+    
+  }
+  console.log(results)
+  return results
+
+}
 
 module.exports = matrix;
